29509a4f5ea955e5d122957812e70fb46ab5eb68
Alexbladexz5 <alexbladexz5@hotmail.com>
2022-02-02 09:56:32 +0100

    Tailwind CSS instalado
    Co-authored-by: Poiki <Poiki@users.noreply.github.com>

--- resources/views/layouts/app/partials/head.blade.php
+++ resources/views/layouts/app/partials/head.blade.php
@@ -4,6 +4,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <link href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" rel="stylesheet" type="text/css">
     <link href="{{ asset('/css/app/wave.css') }}" rel="stylesheet" type="text/css" media="all">
-    <link href="{{asset('/vendor/index/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
     
 </head>
